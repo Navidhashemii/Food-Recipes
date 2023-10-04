4f15bf717bbb7b1b9fc22024e20641399cbc0c47
Navid Hashemi <nhashemi007@yahoo.com>
2023-10-04 21:19:35 +0330

    finish Create component with styles

--- src/pages/Create/Create.js
+++ src/pages/Create/Create.js
@@ -1,9 +1,51 @@
+import { useState } from 'react'
 import './Create.css'
 
 
 function Create() {
+
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [cookingTime, setCookingTime] = useState('')
+
+
   return (
-    <div>
+    <div className='create'>
+      <h2 className='page-title'>Add New Recepies</h2>
+
+      <form>
+        <label>
+          <span>Recepie Title:</span>
+          <input 
+            type='text'
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Recepie Method: </span>
+          <textarea
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Cooking Time(m): </span>
+          <input
+            type='number'
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+            className='ctime'
+          />
+        </label>
+
+        <button className='btn'>Submit</button>
+      </form>
 
     </div>
   )
