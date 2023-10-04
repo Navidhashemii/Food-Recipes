649c1bd40583f523f573a013d2f92f4bd7bfd1b6
Navid Hashemi <nhashemi007@yahoo.com>
2023-10-04 13:23:36 +0330

    fetch data and show in home component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,15 @@
+import { useFetch } from '../../hooks/useFetch'
 import './Home.css'
 
 
 function Home() {
+
+  const {data, isLoading, error} = useFetch('http://localhost:3000/recipes')
   return (
-    <div>
+    <div className='home'>
+      {error && <p className='error'>{error}</p>}
+      {isLoading && <p className='loading'>Loading ...</p>}
+      {data && <RecepieList/>}
 
     </div>
   )
