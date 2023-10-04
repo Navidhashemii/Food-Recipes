8b87a09167b917de154b55005c9ec84b184c93c7
Navid Hashemi <nhashemi007@yahoo.com>
2023-10-04 19:55:59 +0330

    finish Recepie component without styles

--- src/pages/Recepie/Recepie.js
+++ src/pages/Recepie/Recepie.js
@@ -1,9 +1,31 @@
+import { useFetch } from '../../hooks/useFetch'
+import { useParams } from 'react-router-dom'
 import './Recepie.css'
 
 
 function Recepie() {
+  const {id} = useParams()
+  const {data : recepie, isLoading, error} = useFetch(`http://localhost:3000/recipes/${id}`)
+
   return (
-    <div>
+    <div className='recepie'>
+      {error && <p className='error'>{error}</p>}
+      {isLoading && <p className='loading'>Loading ...</p>}
+      {recepie && (
+        <div>
+          <h2>{recepie.title}</h2>
+          <p>Takes {recepie.cookingTime} to cook.</p>
+
+          <ul>
+            {recepie.ingredients.map(ingredient => ( 
+            <li key={ingredient}> {ingredient} </li>
+            ))}
+          </ul>
+          
+          <p>{recepie.method}</p>
+
+        </div>
+        )}
 
     </div>
   )
